1046f986e03db4dd6b9f1bc802392f8462afbee1
Exarcun <32712102+Exarcun@users.noreply.github.com>
2024-03-04 15:47:54 +0100

    Update scrappy.py

--- scrappy.py
+++ scrappy.py
@@ -6,10 +6,9 @@ import logging
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Google search URL
-GOOGLE_SEARCH_URL = 'https://www.google.com/search'
-
-
+..
+..
+..
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
