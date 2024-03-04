cc49609803e13186f483be4edb8ccc9867a7d99b
Exarcun <32712102+Exarcun@users.noreply.github.com>
2024-03-04 15:47:31 +0100

    Update scrappy.py

--- scrappy.py
+++ scrappy.py
@@ -9,11 +9,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 # Google search URL
 GOOGLE_SEARCH_URL = 'https://www.google.com/search'
 
-$$$$$$$$$$$$$
-$$$HIRE ME$$$
-$$$$$$$$$$$$$
 
-# Headers to simulate a real user visit
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
